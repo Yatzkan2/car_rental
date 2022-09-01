e628b70dfd7fe382f1b2714e9b05758054d2dd81
Yatzkan <yyatzkan@gmail.com>
2022-09-01 08:45:36 +0300

    added my data library path

--- car_rental/Registration.cs
+++ car_rental/Registration.cs
@@ -15,7 +15,10 @@ namespace car_rental
     public partial class Registration : Form
     {
 
-         string file = @"C:\Users\IMOE001\Source\Repos\car_rental99\car_rental\Data\UserNameInput.txt";
+        //string file = @"C:\Users\IMOE001\Source\Repos\car_rental99\car_rental\Data\UserNameInput.txt";
+        string file = @"C:\Users\Yair\Desktop\car rental backup\car_rental-master\car_rental-master\car_rental\Data\UserNameInput.txt";
+       
+
         List<string> Useres = new List<string>();
         private bool flagPickedCar = false;
 
